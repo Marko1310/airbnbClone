42c012cebacd9f88ad33121f7911186424e30e97
Marko1310 <marko.cabo13@gmail.com>
2022-12-03 16:27:18 +0100

    Created Card component, passing props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Hero />
+      {/* <Hero /> */}
+      <Card
+        img="katie-zaferes.png"
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life lesson with Katie Zafares"
+        price={136}
+      />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,29 @@
+import katie from "../images/katie-zaferes.png";
+import star from "../images/star.png";
+
+const Card = ({ img, rating, reviewCount, country, title, price }) => {
+  return (
+    <div className="card">
+      <div className="card--status">SOLD OUT</div>
+      <img
+        className="card--image"
+        src={require(`../images/${img}`)}
+        alt="katie"
+      />
+      <div className="card--stats">
+        <img className="card--star" src={star} alt="star" />
+        <span>{rating}</span>
+        <span className="grey">{reviewCount} . </span>
+        <span className="grey">{country}</span>
+      </div>
+
+      <p className="card--descrition">{title}</p>
+      <p className="card--price">
+        {" "}
+        <span className="bold">From ${price}</span> / person
+      </p>
+    </div>
+  );
+};
+
+export default Card;
